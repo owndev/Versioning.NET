1b60104f4c0bb89f1b9f0b329a563a36b2729467
owndev <69784886+owndev@users.noreply.github.com>
2024-06-28 10:15:40 +0200

    Delete Uno Platform directory

--- Uno Platform/PostCommitEdit.ps1
+++
@@ -1,7 +0,0 @@
-#.commit File wird erstellt um eine loop zuverhindern
-if(Test-Path ".commit")
-{
-    Remove-Item ".commit" -Force
-    git add . #Alle geänderten Files in git hinzufügen
-    git commit --amend -C HEAD --no-verify #Letzter Commit bearbeiten ohne Hooks
-}
